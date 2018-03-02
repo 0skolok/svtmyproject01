213f42fa964d952cd5a9d74a81726a1c18b2670f
Victor Soprunov <sovik121@rambler.ru>
2018-03-02 22:44:36 +0300

    Исправлен шаблон новостей на главной

--- local/templates/edu/components/bitrix/news.list/main/template.php
+++ local/templates/edu/components/bitrix/news.list/main/template.php
@@ -10,18 +10,22 @@
 	<? endif; ?>
 
 	<div class="row">
-		<? foreach ($arResult['ITEMS'] as $arItemsId => $arItems): ?>
+		<? foreach ($arResult['ITEMS'] as $arItemId => $arItem): ?>
+			<?
+			$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+			?>
 			<!-- Marketing Icons Section -->
-			<div class="col-lg-4 mb-4">
+			<div class="col-lg-4 mb-4" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 				<div class="card h-100">
-					<h4 class="card-header"><?=$arItems['NAME']?></h4>
+					<h4 class="card-header"><?=$arItem['NAME']?></h4>
 					<div class="card-body">
 						<p class="card-text">
-							<?=TruncateText($arItems['PREVIEW_TEXT'], 230)?>
+							<?=TruncateText($arItem['PREVIEW_TEXT'], 230)?>
 						</p>
 					</div>
 					<div class="card-footer">
-						<a href="<?=$arItems['DETAIL_PAGE_URL']?>" class="btn btn-primary">Читать далее</a>
+						<a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="btn btn-primary">Читать далее</a>
 					</div>
 				</div>
 			</div>
