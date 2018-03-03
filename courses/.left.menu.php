abfcc2a44cf4ce3bea89d652f022fad794854bc8
Victor Soprunov <sovik121@rambler.ru>
2018-03-04 00:44:55 +0300

    Обновлено меню раздела курсов

--- courses/.left.menu.php
+++ courses/.left.menu.php
@@ -1,16 +1,15 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 $aMenuLinks = Array(
 	Array(
 		"Мои курсы",
-		"",
-		Array(), 
-		Array(), 
-		"" 
+		"/courses/my-courses/",
+		Array(),
+		Array(),
+		""
 	),
 	Array(
-		"Список курсов",
-		"",
+		"Каталог курсов",
+		"/courses/catalog/", 
 		Array(), 
 		Array(), 
 		"" 
