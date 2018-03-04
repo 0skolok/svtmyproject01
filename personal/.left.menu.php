afd47efb1098aad070d1614e160e7b1f6516fcc2
Victor Soprunov <sovik121@rambler.ru>
2018-03-04 19:32:13 +0300

    Добавлено меню личного кабинета

---
+++ personal/.left.menu.php
@@ -0,0 +1,54 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Текущие заказы",
+		"/personal/orders/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Личный счет",
+		"/personal/account/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Личные данные",
+		"/personal/private/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"История заказов",
+		"/personal/orders/?filter_history=Y",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Профили заказов",
+		"/personal/profiles/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Корзина",
+		"/personal/cart/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Контакты",
+		"/personal/contacts/",
+		Array(),
+		Array(),
+		""
+	),
+
+);
+?>
